a497334ba1b52ffbe6488d2632676ea9f487a444
Sudhangini Singh <suddhi1515@gmail.com>
2022-06-26 17:32:07 +0530

    node

---
+++ src/Tasks.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Task from './Task';
+import './Tasks.css';
+
+function Tasks(props) {
+  return (
+    <ul className="Tasks__container">
+      {props.tasks.map((task, index) => (
+        <Task
+          key={index}
+          index={index}
+          name={task.name}
+          deadline={task.deadline}
+          category={task.category}
+          completed={task.completed}
+          onClickEdit={props.onClickEdit}
+          onClickDelete={props.onClickDelete}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default Tasks;
+
+Tasks.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    deadline: PropTypes.string,
+    category: PropTypes.string,
+    completed: PropTypes.bool,
+  }).isRequired),
+  onClickEdit: PropTypes.func.isRequired,
+  onClickDelete: PropTypes.func.isRequired,
+};
+
+Tasks.defaultProps = {
+  tasks: [],
+};
